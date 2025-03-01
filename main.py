9d43986f2272c0f2f4a3cfc721346a68c9864226
Алексей Главатских <a@Aleksejs-MacBook-Pro.local>
2025-03-01 21:29:15 +0700

    added colored syntax

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import tkinter
+import re
 from turtle import RawTurtle
+import turtle
+import inspect
+
 
 window = tkinter.Tk(screenName="Turtle")
 
@@ -42,15 +46,13 @@ def run(commands: str):
         for command in final_commands:
             eval(f'tr.{command}')
     except Exception as e:
-        show_error(e)
-
+        show_error(str(e))
 
 entry = tkinter.Text(window, width=40, height=10, padx=10, pady=10, font=("Arial", 16), undo=True)
 entry.grid(padx=4, pady=4, row=0, column=0, rowspan=9, sticky='nsew')
 
-button = tkinter.Button(window, text="Run", command=lambda: run(entry.get("1.0", "end")))
+button = tkinter.Button(window, text="Run", command=lambda: run(entry.get("1.0", "end")), bg='gray', fg="green")
 button.grid(padx=4, pady=4, row=9, column=0, sticky='nsew')
-
 
 def undo_text(event=None):
     entry.edit_undo()
@@ -60,10 +62,32 @@ def delete_text(event=None):
 
 window.bind('<Control-z>', undo_text)
 window.bind('<Control-x>', delete_text)
-
 window.bind('<F5>', lambda event: run(entry.get("1.0", "end")))
 
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
+
+def highlight_syntax(event=None):
+    text = entry.get("1.0", "end-1c")
+
+    entry.tag_remove("keyword", "1.0", "end")
+    entry.tag_remove("number", "1.0", "end")
+
+    keywords = [name for name, obj in inspect.getmembers(turtle, inspect.isfunction)]
+    keywords.append("repeat")
+    keywords.append("endrepeat")
+    pattern_keywords = r'\b(' + '|'.join(keywords) + r')\b'
+    pattern_numbers = r'\b\d+\b'
+
+    for match in re.finditer(pattern_keywords, text):
+        entry.tag_add("keyword", f"1.0 + {match.start()} chars", f"1.0 + {match.end()} chars")
+
+    for match in re.finditer(pattern_numbers, text):
+        entry.tag_add("number", f"1.0 + {match.start()} chars", f"1.0 + {match.end()} chars")
+
+    entry.tag_config("keyword", foreground="green")
+    entry.tag_config("number", foreground="red") 
+
+entry.bind("<KeyRelease>", highlight_syntax)
 
 window.mainloop()
